80753d937b8c5a9496b3b8705993cb36d7af68c4
valentinvs <valentin_vs@hotmail.com>
2015-10-28 22:11:24 +0200

    Fixes: - Implemented game restart when a win occur - Win message implemented twice - The board is rendered dynamically

--- GamePortal/GamePortal.WebSite/Scripts/Games/TicTacToe.js
+++ GamePortal/GamePortal.WebSite/Scripts/Games/TicTacToe.js
@@ -1,89 +1,120 @@
-﻿function getMarkedCellsCountBySign(cells, sign) {
-    var signCount = 0;
+﻿function cellsMatchingSignCount(cells, sign) {
+    var cellsMatchingCount = 0;
 
     $(cells).each(function () {
         var className = this.className;
 
         if (className.indexOf(sign) != -1) {
-            signCount++;
+            cellsMatchingCount++;
         }
     });
 
-    return signCount;
+    return cellsMatchingCount;
 }
 
-function checkIfSignMatchThreeTimes(cells, sign) {
-    var signCount = getMarkedCellsCountBySign(cells, sign);
+function checkSetOfCellsForWin(cellSet) {
+    if (cellsMatchingSignCount(cellSet, 'x') == 3) {
+        alert('Player wins');
+        return true;
+    }
 
-    if (signCount == 3) {
+    if (cellsMatchingSignCount(cellSet, 'o') == 3) {
+        alert('Computer wins');
         return true;
     }
 
     return false;
 }
 
-function checkSetOfCellsForWin(cellSet) {
-    if (checkIfSignMatchThreeTimes(cellSet, 'x')) {
-        alert('Player wins');
-        return;
-    }
-
-    if (checkIfSignMatchThreeTimes(cellSet, 'o')) {
-        alert('Computer wins');
-        return;
-    }
-}
-
 function checkForWin() {
     var row1Cells = $('.r1').children();
-    checkSetOfCellsForWin(row1Cells);
+    if (checkSetOfCellsForWin(row1Cells)) return true;
 
     var row2Cells = $('.r2').children();
-    checkSetOfCellsForWin(row2Cells);
+    if (checkSetOfCellsForWin(row2Cells)) return true;
 
     var row3Cells = $('.r3').children();
-    checkSetOfCellsForWin(row3Cells);
+    if (checkSetOfCellsForWin(row3Cells)) return true;
 
     var column1Cells = $('.c1');
-    checkSetOfCellsForWin(column1Cells);
+    if (checkSetOfCellsForWin(column1Cells)) return true;
 
     var column2Cells = $('.c2');
-    checkSetOfCellsForWin(column2Cells);
+    if (checkSetOfCellsForWin(column2Cells)) return true;
 
     var column3Cells = $('.c3');
-    checkSetOfCellsForWin(column3Cells);
+    if (checkSetOfCellsForWin(column3Cells)) return true;
 
     var upperLeftBottomRight = [];
     upperLeftBottomRight.push(row1Cells[0]);
     upperLeftBottomRight.push(row2Cells[1]);
     upperLeftBottomRight.push(row3Cells[2]);
-    checkSetOfCellsForWin(upperLeftBottomRight);
+    if (checkSetOfCellsForWin(upperLeftBottomRight)) return true;
 
     var upperRightBottomLeft = [];
     upperRightBottomLeft.push(row1Cells[2]);
     upperRightBottomLeft.push(row2Cells[1]);
     upperRightBottomLeft.push(row3Cells[0]);
-    checkSetOfCellsForWin(upperRightBottomLeft);
+    if (checkSetOfCellsForWin(upperRightBottomLeft)) return true;
 }
 
-var itsXturn = true;
-var allCells = $('.cell');
-allCells.each(function (index) {
-    var that = this;
-    $(that).click(function () {
-        if (that.className.indexOf('x') != -1 ||
-            that.className.indexOf('o') != -1) {
-            return;
-        }
+function setSignToCell(cell) {
+    if (cell.className.indexOf('x') != -1 ||
+            cell.className.indexOf('o') != -1) {
+        return;
+    }
 
-        if (itsXturn) {
-            $(that).toggleClass('x');
-            itsXturn = false;
-        } else {
-            $(that).toggleClass('o');
-            itsXturn = true;
-        }
+    if (ItsXturn) {
+        $(cell).toggleClass('x');
+        ItsXturn = false;
+    } else {
+        $(cell).toggleClass('o');
+        ItsXturn = true;
+    }
+}
 
-        checkForWin();
+function resetGame() {
+    var cells = $('.cell');
+
+    cells.each(function () {
+        $(this).removeClass('x');
+        $(this).removeClass('o');
     });
-});
+
+    ItsXturn = true;
+}
+
+function renderBoard() {
+    var row1 = $("<div>", { class: "r1" });
+    row1.append($('<div>', { class: "cell c1" }), $('<div>', { class: "cell c2" }), $('<div>', { class: "cell c3" }))
+
+    var row2 = $("<div>", { class: "r2" });
+    row2.append($('<div>', { class: "cell c1" }), $('<div>', { class: "cell c2" }), $('<div>', { class: "cell c3" }))
+
+    var row3 = $("<div>", { class: "r3" });
+    row3.append($('<div>', { class: "cell c1" }), $('<div>', { class: "cell c2" }), $('<div>', { class: "cell c3" }))
+
+    GameContainer.append(row1,row2,row3);
+}
+
+var ItsXturn = true;
+var GameContainer = $('#GameContainer');
+
+function startGame() {
+    renderBoard();
+    
+    var allCells = $('.cell');
+
+    allCells.each(function (index) {
+        var that = this;
+        $(that).click(function () {
+            setSignToCell(that);
+            if (checkForWin()) {
+                resetGame();
+                return false;
+            }
+        });
+    });
+}
+
+startGame();
